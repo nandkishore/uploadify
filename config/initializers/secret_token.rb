982e4e2ce0cbd3f85996bf153722abc6534321e5
nandkishore <nandkishore18@gmail.com>
2013-05-08 12:33:10 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uploadify::Application.config.secret_token = 'f46d28e7794dd79ceb1642b4734514d1a126656575eb5e680b03a0dd1db3286ed6d0ac6843d76353500c429d6d1d51a6c9a8e25a3d0cc413bb7fbfeee1f8a7c5'
